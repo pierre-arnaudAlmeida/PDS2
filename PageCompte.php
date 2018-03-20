88ebb8a79f5d6a763b6e068498c7f652ac0849c2
pierre-arnaudAlmeida <37550509+pierre-arnaudAlmeida@users.noreply.github.com>
2018-03-20 17:04:55 +0100

    v0.03
    Footer + Accueil + main

--- PageCompte.php
+++ PageCompte.php
@@ -21,7 +21,5 @@ session_start();
 			</div>
 		</div>
 </body>
-<footer>
-		<iframe id="Carte" src="https://www.google.com/maps/embed?pb=!4v1521552283461!6m8!1m7!1sYQvqLT2odncNyffTnkzO5g!2m2!1d48.79518384236498!2d2.443999342281539!3f234.59633!4f0!5f0.7820865974627469" width="600" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>
-	</footer>
+	<?php include("Footer.php"); ?>
 </html>
--- PageConnexion.php
+++ PageConnexion.php
@@ -12,7 +12,7 @@ session_start()
 <body>
 	<?php include("Header.php"); ?>
 
-		<div id="pagecentrale">
+		<div id="pageconnexion">
 			<?php include("Connexion.php"); ?>
 			<div id="Nav">
 				<h1>Vous avez déja un compte alors il ne vous reste plus qu'à vous connecté.</br></br>
@@ -22,7 +22,5 @@ session_start()
 		</div>
 		</div>
 </body>
-<footer>
-		<iframe id="Carte" src="https://www.google.com/maps/embed?pb=!4v1521552283461!6m8!1m7!1sYQvqLT2odncNyffTnkzO5g!2m2!1d48.79518384236498!2d2.443999342281539!3f234.59633!4f0!5f0.7820865974627469" width="600" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>
-	</footer>
+	<?php include("Footer.php"); ?>
 </html>
