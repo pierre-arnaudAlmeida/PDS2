149ec8a9752dfa01bda2e8b817856fb937070254
pierre-arnaudAlmeida <37550509+pierre-arnaudAlmeida@users.noreply.github.com>
2018-03-21 12:18:29 +0100

    v0.06
    Footer + CSS

--- Accueil.php
+++ Accueil.php
@@ -17,7 +17,14 @@
 <body>
 	<?php include("Header.php"); ?>
 
-		<div id="pagecentrale">
+		<div id="pageaccueil">
+			<div id="Nav2">
+				<h5>Bienvenue sur DEVICE MANAGER</h5>
+					<h1>
+					Vous avez envie de géré vos objets connnectés directement en ligne ?<br><br>
+					Et bien vous êtes sur le premier site en France permettant de le faire en toute simplicité. Grace à notre site vous pourrez gérer différents types d'objets tels que votre télévision, vos volets ou bien pourquoi pas controlé l'integralité de votre éclairage totalement en ligne grace à nous.
+				</h1>
+			</div>
 			<script language="javascript">StartAnim();</script>
 		</div>
 </body>
--- Footer.php
+++ Footer.php
@@ -1,22 +1,22 @@
 <footer>
 		<iframe id="Carte" src="https://www.google.com/maps/embed?pb=!4v1521552283461!6m8!1m7!1sYQvqLT2odncNyffTnkzO5g!2m2!1d48.79518384236498!2d2.443999342281539!3f234.59633!4f0!5f0.7820865974627469" width="600" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>
 		
-		
 		<div id="Lien">
-
 			<h2>Menu</h2>
-			<a href="Accueil.php"><a2>- Accueil</a2></a>
-			<a href="PageObjets.php"><a2>- Objets</a2></a>
-			<a href="PageConnexion.php"><a2>- Connexion</a2></a>
-			<a href="PageInscription.php"><a2>- S'inscrire</a2></a>
-			
+			<a href="Accueil.php"><image src="http://www.arinluzien.fr/fs/Root/d7mz7-fleche_noire.png" width="20" height="20"></image><a2> Accueil</a2></a>
+			<a href="PageObjets.php"><image src="http://www.arinluzien.fr/fs/Root/d7mz7-fleche_noire.png" width="20" height="20"></image><a2> Objets</a2></a>
+			<a href="PageConnexion.php"><image src="http://www.arinluzien.fr/fs/Root/d7mz7-fleche_noire.png" width="20" height="20"></image><a2> Connexion</a2></a>
+			<a href="PageInscription.php"><image src="http://www.arinluzien.fr/fs/Root/d7mz7-fleche_noire.png" width="20" height="20"></image><a2> S'inscrire</a2></a>
+		</div>
 
-			<image src="https://medias.audiofanzine.com/images/normal/headway-guitars-1849271.png" width="80" height="80"></image>
+		<div id="reseaux">
+			<a href="https://www.facebook.com/"><image src="http://leglouton-foodtruck.com/wp-content/uploads/2014/02/logofacebook.png" width="60" height="60"></image></a>
+ 			
+ 			<a href="https://twitter.com/"><image src="https://i.pinimg.com/originals/5c/a9/8c/5ca98c73b2bb7a02bf8350933c7ca443.png" width="60" height="60"></image></a>
 
-  			<script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+ 			<a href="https://www.linkedin.com/"><image src="https://icon-icons.com/icons2/693/PNG/512/LinkedIn_Rounded_Solid_icon-icons.com_61559.png" width="60" height="60"></image></a>
 
-  			<a href="http://twitter.com/share" class="twitter-share-button" 
- 			data-count="vertical" data-via="InfoWebMaster">Tweet</a>
+ 			<a href="https://instagram.com/"><image src="http://blog.louisantoinette.com/wp-content/uploads/2016/05/logo-instagram.png" width="60" height="60"></image></a>
 
 		</div>
 </footer>
