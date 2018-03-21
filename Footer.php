73ec5820941401a08b4441bdf74e93e5de9f944e
Keita97 <37551410+Keita97@users.noreply.github.com>
2018-03-21 10:13:58 +0100

    v0.05
    Changer le main et le footer

--- Footer.php
+++ Footer.php
@@ -1,21 +1,22 @@
 <footer>
 		<iframe id="Carte" src="https://www.google.com/maps/embed?pb=!4v1521552283461!6m8!1m7!1sYQvqLT2odncNyffTnkzO5g!2m2!1d48.79518384236498!2d2.443999342281539!3f234.59633!4f0!5f0.7820865974627469" width="600" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>
 		
+		
 		<div id="Lien">
+
+			<h2>Menu</h2>
 			<a href="Accueil.php"><a2>- Accueil</a2></a>
 			<a href="PageObjets.php"><a2>- Objets</a2></a>
 			<a href="PageConnexion.php"><a2>- Connexion</a2></a>
 			<a href="PageInscription.php"><a2>- S'inscrire</a2></a>
-
-
 			
 
-			<iframe src="http://www.facebook.com/plugins/like.php?href=http://www.example.com/page.html&layout=box_count&show_faces=true&width=65&action=like&font=arial&colorscheme=light&height=65" 
-  			scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:65px; height:65px; margin-top:3px;" allowTransparency="true"></iframe>
+			<image src="https://medias.audiofanzine.com/images/normal/headway-guitars-1849271.png" width="80" height="80"></image>
 
   			<script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
 
   			<a href="http://twitter.com/share" class="twitter-share-button" 
  			data-count="vertical" data-via="InfoWebMaster">Tweet</a>
+
 		</div>
-	</footer>
+</footer>
